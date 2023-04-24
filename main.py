ac196b2466a1b53a1714733b6a178942e4c9ce74
InVorker <120738638+InWorker1@users.noreply.github.com>
2023-04-25 01:07:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,35 @@ import telebot
 from datetime import datetime
 from time import sleep
 from telebot import types
-bot = telebot.TeleBot('токен')
-# from KeyBoard import markup
 
+bot = telebot.TeleBot('5540761456:AAHbhGv1J1dpV3ygCCmGCAncrYam05gdE-s')
 
 @bot.message_handler(commands=["start"])    #начальная комманда старта
 def start(message):
-    markup=types.InlineKeyboardMarkup(row_width=1)
+
+    inline_markup=types.InlineKeyboardMarkup(row_width=1)
     item=types.InlineKeyboardButton('создай список покупок', callback_data='buy_list_bt')
     item2=types.InlineKeyboardButton('начни следить за временем', callback_data='time_lesson_bt')
-    markup.add(item,item2)
-    bot.send_message(message.chat.id, 'привет, друг!', reply_markup=markup)
+    inline_markup.add(item,item2)
+    bot.send_message(message.chat.id, 'привет, друг!', reply_markup=inline_markup)
     # bot.register_next_step_handler(message, time_lesson)
 
 @bot.callback_query_handler(func=lambda call: True)       #комманда ответа на кнопку
 def callback(call):
-    if call.message:
-        if call.data=='buy_list_bt':
-            bot.send_message(call.message.chat.id, 'перечисляй продукты в одну строку через пробел')
+    match call.data:
+        case 'buy_list_bt':
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='перечисляй продукты в одну строку через пробел')
             bot.register_next_step_handler(call.message, list_buy)
-        if call.data=='tiem_lesson_bt':
-            bot.send_message(call.message.chat.id, 'хорошо я слежу за временем, иди пока отдохни, а я напишу за 3 минуты до урока')
+        case 'tiem_lesson_bt':
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='хорошо я слежу за временем, иди пока отдохни, а я напишу за 3 минуты до урока')
             bot.register_next_step_handler(call.message, time_lesson)
+        case 'delete':     #callback который отвечает за удаление сообщения бота по нажатию кнопки
+            bot.delete_message(call.message.chat.id, call.message.message_id )
+        case 'restart':
+            for i in range(count_lb+2):
+                bot.delete_message(call.message.chat.id, call.message.message_id - i)
+            start(call.message)
+
 
 @bot.message_handler(content_types=['text'])      #ответы и шаги после определенного сообщения
 def handler_text(message):
@@ -35,9 +42,10 @@ def handler_text(message):
             case 'факториал':
                 bot.send_message(message.chat.id, 'напиши число')
                 bot.register_next_step_handler(message, factorial)
+            case 'привет':
+                start(message)
     except:
         bot.send_message(message.chat.id, 'не понял вас. посмотрите комманды и повторите, что хотели мне сказать')
-
 
 def time_lesson(message):
     global dt_now
@@ -61,14 +69,24 @@ def time_lesson(message):
 
         sleep(60)
 
-
 def list_buy(message):
+    global count_lb
     mes = message.text.split()
-    count = 0
-    for i in mes:
-        if type(i) == str and i != '':
-            count += 1
-            bot.send_message(message.chat.id, f'{count}) {i}')
+    count_lb = 0
+    lb_markup = types.InlineKeyboardMarkup(row_width=1)
+    lbbt = types.InlineKeyboardButton('удалить из списка', callback_data='delete')
+    lb_markup.add(lbbt)
+    for i in range(len(mes)):
+        if type(mes[i]) == str and mes[i] != '':
+            count_lb += 1
+            if count_lb == len(mes):
+                restart_lb=types.InlineKeyboardButton('вернуться к выбору команды', callback_data='restart')
+                lb_markup.add(restart_lb)
+                bot.send_message(message.chat.id, f'{count_lb}) {mes[i]}', reply_markup=lb_markup)
+                break
+            bot.send_message(message.chat.id, f'{count_lb}) {mes[i]}', reply_markup=lb_markup)
+
+
 
 def factorial(message):
     cif = int(message.text.strip())
@@ -76,7 +94,6 @@ def factorial(message):
     for i in range(1, cif + 1):
         counter *= i
     bot.send_message(message.chat.id, f'{counter}')
-
 
 while True:
     try:
